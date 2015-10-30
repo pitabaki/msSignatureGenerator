c69b2ef43c4ac4cd208c49b3f50c2ae1bef0727c
Peter Berki <pberki1986@gmail.com>
2015-10-30 13:05:28 -0700

    Form works with multiple numbers. SASS updated to reflect functionality.

--- js/conversion.js
+++ js/conversion.js
@@ -9,6 +9,8 @@ Developed for: Meyer Sound
 
 //replace text function
 function textSwap(id,message){
+	//measure taken in reducing sloppy coding
+	'use strict';
 	var output = document.getElementById(id);
 	if(output.textContent !== 'undefined'){
 		output.textContent = message;
@@ -19,20 +21,43 @@ function textSwap(id,message){
 
 //Defined init Function (onload, do this)
 function init(){
+	//measure taken in reducing sloppy coding
 	'use strict';
-	//Create Variables
+	//Create Variables (DOM Selectors)
 	var name = document.getElementById("name"),
 		position = document.getElementById("position"),
 		number = document.getElementById("number"),
+		number2 = document.getElementById("number2"),
+		phoneNumber = document.getElementById("phoneNumber"),
 		email = document.getElementById("email"),
 		form = document.getElementById("form"),
+		numOne = document.getElementById("numOne"),
+		numTwo = document.getElementById("numTwo"),
+		numSwitch = document.getElementById("numSwitch"),
 		rplName = "rplName",
 		rplTitle = "rplTitle",
 		rplNum1 = "rplNum1",
 		rplNum2 = "rplNum2",
 		rplEmail = "rplEmail",
-		anchor = document.getElementsByTagName('a'),
+		anchor = document.getElementsByTagName("a"),
 		emHREF = anchor[1];
+
+	//when amount of numbers is changed
+	function numChange(){
+		if(numSwitch.selectedIndex === 1){
+			numOne.style.display = "none";
+			phoneNumber.innerText = "Phone Numbers:";
+			number2.style.display = "block";
+			numTwo.style.display = "block";
+		}else{
+			numTwo.style.display = "none";
+			number2.style.display = "none";
+			phoneNumber.innerText = "Phone Number:";
+			numOne.style.display = "block";
+		}
+	}
+
+	numSwitch.onchange = numChange;
 
 	//on submit
 	function fluxCapacitate(){
