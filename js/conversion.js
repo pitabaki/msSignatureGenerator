2fe30aa28f5aad66b9456caf5e6868cea48b063c
Peter Berki <pberki1986@gmail.com>
2015-11-20 18:24:46 -0800

    setTimeout now available to animation functions.

--- js/conversion.js
+++ js/conversion.js
@@ -27,12 +27,31 @@ function inputHTML(id,property){
 	output.innerHTML = property;
 }
 
-function fadedBG(id,hue,cloak){
+function fadedBG(id,hue,cloak,time){
 	//measure taken in reducing sloppy coding
 	'use strict';
 	var bgDiv = document.getElementById(id);
-	bgDiv.style.display = cloak;
-	bgDiv.style.backgroundColor = hue;
+	if(typeof time === 'undefined'){
+		bgDiv.style.display = cloak;
+		setTimeout(function(){
+			bgDiv.style.backgroundColor = hue;
+		},50);
+	}else{
+		bgDiv.style.backgroundColor = hue;
+		setTimeout(function(){
+			bgDiv.style.display = cloak;
+		},time);
+	}
+}
+
+function whatUp(id,position,time){
+	//measure taken in reducing sloppy coding
+	'use strict';
+	var bgDiv = document.getElementById(id);
+	setTimeout(function(){
+		bgDiv.style.marginTop = position;
+	},time);
+	
 
 }
 
@@ -52,7 +71,8 @@ function init(){
 		formPos = document.getElementById("padTop"),
 		close = document.getElementById("close-button"),
 		blackBG = document.getElementById("blackBG"),
-		coloring = "rgba(0,0,0,0.5)",
+		coloring = "rgba(0,0,0,0.75)",
+		erasing = "rgba(0,0,0,0)",
 		htmlBR = "<br />",
 		breakInsert = document.getElementById("break"),
 		breakInsert2 = document.getElementById("break2"),
@@ -65,7 +85,6 @@ function init(){
 		anchor = document.getElementsByTagName("a"),
 		emHREF = anchor[0]; //References signature's # email address
 		//Will replace # with whatever user enters
-
 	//on submit
 	function fluxCapacitate(){
 		name = name.value;
@@ -74,9 +93,12 @@ function init(){
 		number2 = "T: " + number2.value;
 		number3 = "T: " + number3.value;
 		email = email.value;
+		email = email.toLowerCase();
+		console.log(email);
 		emHREF.href = "mailto:" + email;
 		formPos.style.left = 0;
 		fadedBG("blackBG", coloring, "block");
+		whatUp("copyPaste","0", 250);
 		if((name.length > 0) && (position.length >0) && (number.length > 0) && (number2.length > 3) && (number3.length > 3) && (email.length > 0)){
 			textSwap(rplName,name);
 			textSwap(rplTitle,position);
@@ -113,12 +135,15 @@ function init(){
 	function deAtomize(){
 		if(formPos.style.left === "0px"){
 			formPos.style.left = "-600px";
-			fadedBG("blackBG",coloring,"none");
+			fadedBG("blackBG",erasing,"none",500);
+			whatUp("copyPaste","-150px", 250);
 		}else{
 			alert("FAIL!!!");//Just reassurance I haven't completely screwed up
 		}
 	}
+	/*function sigCopy(){
 
+	}*/
 	close.onclick = deAtomize;
 	blackBG.onclick = deAtomize;
 	form.onsubmit = fluxCapacitate;
